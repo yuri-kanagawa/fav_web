37973c78f82dfbfb7ed77a5fa12ff2868a5ac3ec
yuri-kanagawa <nightmare37998@icloud.com>
2021-10-30 17:40:37 +0900

    django test complete

--- backend/web-back/fav/tests.py
+++ backend/web-back/fav/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from .models import Fav
+
+
+class FavModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Fav.objects.create(title="first fav", body="a body here")
+
+    def test_title_content(self):
+        fav = Fav.objects.get(id=1)
+        excepted_object_name = f'{fav.title}'
+        self.assertEqual(excepted_object_name, 'first fav')
+
+    def test_body_content(self):
+        fav = Fav.objects.get(id=1)
+        excepted_object_name = f'{fav.body}'
+        self.assertEqual(excepted_object_name, 'a body here')
