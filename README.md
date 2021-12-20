855b3af62925dd14c8d342a7e863c49541acbf87
yuri-kanagawa <nightmare37998@icloud.com>
2021-12-20 21:52:47 +0900

    MinorCorrection

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ https://qiita.com/greenteabiscuit/items/c40ba038703c9f33499b
 tailwind css 導入手順
 https://tailwindcss.com/docs/guides/nextjs
 https://qiita.com/Masahiro111/items/62a4927be42a8c06bc79
+
+#起動コマンド
+docker-composed up
--- frontend/web-front/.next/static/chunks/webpack.js
+++ frontend/web-front/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d67e271ce40d43a3"; }
+/******/ 		__webpack_require__.h = function() { return "e791ca4eb79ede8e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
